42d4ad12ace7828e16b971146eadda051a8429c7
diogo <diogo@LAPTOP-BD4ET3KN>
2020-02-13 17:39:41 +0000

    inicio/criaçao do ficheiro .cpp

---
+++ ex1.cpp
@@ -0,0 +1,19 @@
+// Ciclo de Desenvolvimento de Software
+	// Escrito por: 
+	// Data: 
+
+#include <iostream>
+#include <locale>
+using namespace std;
+
+int main() {
+	locale::global(locale(""));
+	cout << "Os passos para o desenvolvimento de SW seguro são:" << endl;
+	cout << "1. Análise: Definir o problema" << endl;
+	cout << "2. Desenho: Planear a solução" << endl;
+	cout << "3. Implementação: Codificar a solução" << endl;
+	cout << "4. Testar e depurar" << endl;
+	cout << "5. Manter e documentar" << endl;
+	cout << "6. Adicionar mecanismos de segurança" << endl;
+	return 0;
+}
