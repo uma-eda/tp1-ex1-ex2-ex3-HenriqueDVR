bcd1bd6d63041e59c6d78ce130e70d05b7c1e24f
diogo <diogo@LAPTOP-BD4ET3KN>
2020-02-20 16:37:57 +0000

    ex3- adicionar "tendo em conta a segurança" em cada frase

--- ex1.cpp
+++ ex1.cpp
@@ -9,11 +9,11 @@ using namespace std;
 int main() {
 	locale::global(locale(""));
 	cout << "Os passos para o desenvolvimento de SW seguro são:" << endl;
-	cout << "1. Análise: Definir o problema" << endl;
-	cout << "2. Desenho: Planear a solução" << endl;
-	cout << "3. Implementação: Codificar a solução" << endl;
-	cout << "4. Testar e depurar" << endl;
-	cout << "5. Manter e documentar" << endl;
+	cout << "1. Análise: Definir o problema tendo em conta a segurança" << endl;
+	cout << "2. Desenho: Planear a solução tendo em conta a segurança" << endl;
+	cout << "3. Implementação: Codificar a solução tendo em conta a segurança" << endl;
+	cout << "4. Testar e depurar tendo em conta a segurança" << endl;
+	cout << "5. Manter e documentar tendo em conta a segurança" << endl;
 	// cout << "6. Adicionar mecanismos de segurança" << endl;
 	return 0;
 }
